e7292ffc5c472c2501db8dd3de9a85d5fd2578c8
g1992003 <93967767+g1992003@users.noreply.github.com>
2024-03-22 09:53:17 +0800

    更新main.js

--- local/src/js/template/modal.js
+++ local/src/js/template/modal.js
@@ -2,20 +2,19 @@ $('#modalBg').css('display', 'block');
 $('body').addClass('modal-open')
 
 $('#close').on('click', function () {
-$('#modalBg').css('display', 'none')
-$('body').removeClass('modal-open')
-player.stopVideo();
+    $('#modalBg').css('display', 'none')
+    $('body').removeClass('modal-open')
 })
 const outer = document.getElementById('modalBg')
 const inner = document.getElementById('myModal')
 outer.addEventListener("click", function (e) {
-$('#modalBg').css('display', 'none')
-$('body').removeClass('modal-open')
-player.stopVideo();
-e.stopPropagation();
+    $('#modalBg').css('display', 'none')
+    $('body').removeClass('modal-open')
+    player.stopVideo();
+    e.stopPropagation();
 }, false);
 inner.addEventListener('click', function (e) {
-e.stopPropagation();
+    e.stopPropagation();
 }, false);
 $(document).ready(function() {
     $('.modal-open').css('height', $('body').height() + 'px');
